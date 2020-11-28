7e25315af60065d138002b9d1fa2161ec2c5ca79
Talal Bafarat <tbafarat@gmail.com>
2020-11-28 21:17:15 +0300

    tablecreaton

--- sql/createCompetitionTable.php
+++ sql/createCompetitionTable.php
@@ -15,9 +15,19 @@
     $sql = "CREATE TABLE IF NOT EXISTS membership_form(
     membershipid VARCHAR(12) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
     fullname VARCHAR(30),
-    memberrole VARCHAR(15),
-    birthdate VARCHAR(20),
-    phonenumber VARCHAR(12)
+    phonenumber VARCHAR(12),
+    nationality VARCHAR(30),
+    age VARCHAR(2),
+    flightcontrollername VARCHAR(30),
+    framesize VARCHAR(20),
+    escamps VARCHAR(30),
+    motorssizeandkv VARCHAR(30),
+    vtxantennapolarity VARCHAR(30),
+    batterycellsnumber VARCHAR(2),
+    batterycellscapacity VARCHAR(10),
+    overallweight FLOAT(20),
+
+
     )";
     
     //step6 : run the query
