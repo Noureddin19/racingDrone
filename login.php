5c6abb3d373222cddbdc03db0cd1acd8c348ad35
Naif Alfirm <Nfahad40@gmail.com>
2020-12-06 16:52:04 +0300

    a

---
+++ login.php
@@ -0,0 +1,430 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+    <!-- META -->
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="keywords" content="" />
+    <meta name="author" content="" />
+    <meta name="robots" content="" />
+    <meta name="description" content="" />
+
+    <!-- FAVICONS ICON -->
+    <link rel="icon" href="images/favicon.ico" type="image/x-icon" />
+    <link rel="shortcut icon" type="image/x-icon" href="images/client-logo/racinglabWhite.png" />
+
+    <!-- PAGE TITLE HERE -->
+    <title>PSU Drone Racing League - Registration</title>
+
+    <!-- MOBILE SPECIFIC -->
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+
+    <!-- BOOTSTRAP STYLE SHEET -->
+    <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+    <!-- FONTAWESOME STYLE SHEET -->
+    <link rel="stylesheet" type="text/css" href="css/fontawesome/css/font-awesome.min.css" />
+
+    <!-- OWL CAROUSEL STYLE SHEET -->
+    <link rel="stylesheet" type="text/css" href="css/owl.carousel.min.css">
+
+    <!-- SLICK SLIDER STYLE SHEET -->
+    <link rel="stylesheet" type="text/css" href="css/slick.css">
+    <link rel="stylesheet" type="text/css" href="css/slick-theme.css">
+
+    <!-- BOOTSTRAP SLECT BOX STYLE SHEET  -->
+    <link rel="stylesheet" type="text/css" href="css/bootstrap-select.min.css">
+    <!-- MAGNIFIC POPUP STYLE SHEET -->
+    <link rel="stylesheet" type="text/css" href="css/magnific-popup.min.css">
+    <!-- LOADER STYLE SHEET -->
+    <link rel="stylesheet" type="text/css" href="css/loader.min.css">
+    <!-- MAIN STYLE SHEET -->
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+    <!-- FLATICON STYLE SHEET -->
+    <link rel="stylesheet" type="text/css" href="css/flaticon.min.css">
+    <!-- THEME COLOR CHANGE STYLE SHEET -->
+    <link rel="stylesheet" class="skin" type="text/css" href="css/skin/skin-1.css">
+
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+
+    <!-- MOBILE SPECIFIC -->
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+
+    <!-- BOOTSTRAP STYLE SHEET -->
+    <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+    <!-- FONTAWESOME STYLE SHEET -->
+    <link rel="stylesheet" type="text/css" href="css/fontawesome/css/font-awesome.min.css" />
+
+    <!-- OWL CAROUSEL STYLE SHEET -->
+    <link rel="stylesheet" type="text/css" href="css/owl.carousel.min.css">
+
+    <!-- SLICK SLIDER STYLE SHEET -->
+    <link rel="stylesheet" type="text/css" href="css/slick.css">
+    <link rel="stylesheet" type="text/css" href="css/slick-theme.css">
+
+    <!-- BOOTSTRAP SLECT BOX STYLE SHEET  -->
+    <link rel="stylesheet" type="text/css" href="css/bootstrap-select.min.css">
+    <!-- MAGNIFIC POPUP STYLE SHEET -->
+    <link rel="stylesheet" type="text/css" href="css/magnific-popup.min.css">
+    <!-- LOADER STYLE SHEET -->
+    <link rel="stylesheet" type="text/css" href="css/loader.min.css">
+    <!-- MAIN STYLE SHEET -->
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+    <!-- FLATICON STYLE SHEET -->
+    <link rel="stylesheet" type="text/css" href="css/flaticon.min.css">
+    <!-- THEME COLOR CHANGE STYLE SHEET -->
+    <link rel="stylesheet" class="skin" type="text/css" href="css/skin/skin-1.css">
+    <link rel="stylesheet" href="css/racingStyle.css">
+
+
+
+
+
+
+
+
+    <!-- REVOLUTION SLIDER CSS -->
+    <link rel="stylesheet" type="text/css" href="plugins/revolution/revolution/css/settings.css">
+    <!-- REVOLUTION NAVIGATION STYLE -->
+    <link rel="stylesheet" type="text/css" href="plugins/revolution/revolution/css/navigation.css">
+
+    <!-- GOOGLE FONTS -->
+    <link href="https://fonts.googleapis.com/css2?family=Rajdhani:wght@300;400;500;600;700&display=swap"
+        rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Muli:wght@200;300;400;500;600;700;800;900&display=swap"
+        rel="stylesheet">
+
+
+
+
+
+
+
+
+    <!-- REVOLUTION SLIDER CSS -->
+    <link rel="stylesheet" type="text/css" href="plugins/revolution/revolution/css/settings.css">
+    <!-- REVOLUTION NAVIGATION STYLE -->
+    <link rel="stylesheet" type="text/css" href="plugins/revolution/revolution/css/navigation.css">
+
+    <!-- GOOGLE FONTS -->
+    <link href="https://fonts.googleapis.com/css2?family=Rajdhani:wght@300;400;500;600;700&display=swap"
+        rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Muli:wght@200;300;400;500;600;700;800;900&display=swap"
+        rel="stylesheet">
+
+
+</head>
+
+<body>
+
+    <div class="page-wraper">
+
+
+
+        <!-- HEADER START -->
+        <header class="site-header header-style-4 mobile-full-width-menu nav-wide">
+
+            <div class="sticky-header main-bar-wraper  navbar-expand-lg ">
+                <div class="main-bar">
+                    <div class="container-fluid clearfix">
+                    <div class="row mb-4">
+                    <div class="logo-header col-sm-8 w-100 ">
+                            <div class="logo-header-inner logo-header-one  ">
+                            <a class="ml-2" href="index.php"><i class="fa fa-chevron-left  " style="color:white; font-size:25px"></i></a>
+
+                                <a href="index.php">
+                                    <img src="images/client-logo/PSU Drone Racing Club-02.png" alt="" width="300px;">
+                                    </a>
+                                </div>
+                            </div>  
+                                                       
+                            <div class="extra-nav header-2-nav col-sm-4 ">
+                                
+                                <div class="extra-cell ">
+                                    <div class="header-nav-request">
+                                        <a href="mailto:racing@psu.edu.sa" style="background-color: inherit">racing@psu.edu.sa</a> 
+                                    </div>
+                                </div>                                
+                                 
+                             </div>  
+                    </div>
+                                                                                              
+
+                             
+                            <!-- MAIN Vav -->
+                            <div class="nav-animation header-nav navbar-collapse collapse  justify-content-center">
+                        
+    
+                            </div>
+
+                        </div>
+
+
+                       
+                        <!-- SITE Search -->
+                        <div id="search-toggle-block">
+                            <div id="search">
+                                <form role="search" id="searchform" action="/search" method="get" class="radius-xl">
+                                    <div class="input-group">
+                                        <input class="form-control" value="" name="q" type="search"
+                                            placeholder="Type to search" />
+                                        <span class="input-group-append"><button type="button" class="search-btn"><i
+                                                    class="fa fa-search"></i></button></span>
+                                    </div>
+                                </form>
+                            </div>
+                        </div>
+
+                    </div>
+                </div>
+
+            </div>
+
+        </header>
+        <!-- HEADER END -->
+            <div class=" container-fluid pr-0 pl-0 shadow-lg m" >
+                <img src="images/background/the-price-of-a-racing-drone-varies.jpeg" alt="">
+            </div>
+        <!-- CONTENT START -->
+        <div class="page-content container p-5" id="home1" >
+
+        
+
+        <form action="admin.php" method="post" class="was-validated ">
+        <h3>Log in</h3>
+                                                              <div class="form-group text-left">
+                                                                  <label for="uname" class="left">Username</label>
+                                                                  <input type="text" class="form-control border" id="uname" placeholder="Enter Name" name="uname" required>
+                                                                  <div class="valid-feedback">Valid.</div>
+                                                                  <div class="invalid-feedback">Please fill out this field.</div>
+                                                              </div>
+                                                             
+                                                              <div class="form-group text-left">
+                                                                  <label for="pwd">Password</label>
+                                                                  <input type="password" class="form-control border" id="pwd" placeholder="Enter Your Phone Number" name="pn" required>
+                                                                  <div class="valid-feedback">Valid.</div>
+                                                                  <div class="invalid-feedback">Please fill out this field.</div>
+                                                              </div>
+                                                             
+
+                                                              <br>
+                                                           
+                                                             
+                                                              <center>
+                                                              <button type="submit" name="submit" value ="submit" class="site-button site-btn-effect justify-content-center mt-4">Send</button>
+                                                              </center>
+                                                          </form>
+
+
+        </div>
+
+        <!-- CLIENT LOGO SECTION START -->
+        <div class="section-full bg-gray p-t50 p-b50 client-logo-outer-one">
+                <div class="container-fluid">
+
+                    <div class="section-content p-tb30 owl-btn-vertical-center">
+                        <div class="owl-carousel home-client-carousel-2">
+
+                            
+
+                    
+
+                            <div class="item">
+                                <div class="ow-client-logo">
+                                    <div class="client-logo client-logo-media">
+                                        <a href="javascript:void(0);"><img src="images/client-logo/Club Logo.png" alt=""></a>
+                                    </div>
+                                </div>
+                            </div>
+                             <div class="item">
+                                 <div class="ow-client-logo">
+                                     <div class="client-logo client-logo-media">
+                                         <a href="javascript:void(0);"><img
+                                                 src="images/client-logo/PSU Logo.png" alt=""></a>
+                                     </div>
+                                 </div>
+                             </div>
+                             
+                            
+                           
+
+                            <div class="item">
+                                <div class="ow-client-logo">
+                                    <div class="client-logo client-logo-media">
+                                        <a href="javascript:void(0);"><img src="images/client-logo/RIOTU Logo.png" alt=""></a>
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div class="item">
+                                <div class="ow-client-logo">
+                                    <div class="client-logo client-logo-media">
+                                        <a href="javascript:void(0);"><img src="images/client-logo/az logo .png" alt=""></a>
+                                    </div>
+                                </div>
+
+                           
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+            <!-- CLIENT LOGO  SECTION End -->
+            </div>
+        <!-- CONTENT END -->
+
+         <!-- FOOTER START -->
+         <footer class="site-footer footer-large footer-dark text-white footer-style1">
+
+
+<!-- FOOTER BLOCKES START -->
+<div class="footer-top" id="contactus">
+    <div class="container">
+        <div class="row">
+
+            <div class="col-lg-3 col-md-6 " style="margin-right: 15%">
+
+                <div class="widget recent-posts-entry">
+                    <h3 class="widget-title">Get in Touch</h3>
+                    <ul class="widget_address">
+                        <li><i class="fa fa-map-marker"></i>P.O.Box No. 66833 Rafha Street, Riyadh </li>
+                        <li><i class="fa fa-envelope"></i>racing@psu.edu.sa</li>
+                        <li> <i class="fa fa-phone"></i>0114948851</li>
+                    </ul>
+                </div>
+
+
+            </div>
+
+
+            <div class="col-lg-3 col-md-6 "style="margin-right: 10%">
+                <div class="widget widget_services">
+                    <h3 class="widget-title">Useful links</h3>
+                    <ul>
+                    <li><a target="_blank" href="https://www.riotu-lab.org">RIOTU Lab</a></li>
+                        <li><a target="_blank" href="https://www.psu.edu.sa">Prince Sultan University</a></li>
+                    </ul>
+                </div>
+            </div>
+
+            <div class="col-lg-3 col-md-6 " style="margin-right: auto;">
+                <div class="widget recent-posts-entry">
+                    <h3 class="widget-title">Our Location</h3>
+                    <div class="section-content">
+                        <div class="widget-post-bx">
+                            <div class="widget-post clearfix">
+                                
+                               
+                                    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3623.731718429438!2d46.699132215759334!3d24.736089084112688!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3e2f0292c1fb5529%3A0x3fbc5cb308e60dd8!2sPrince%20Sultan%20University!5e0!3m2!1sen!2ssa!4v1604594675336!5m2!1sen!2ssa" width="100%" height="200em" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0" class="shadow-lg"></iframe>
+
+                                
+                            </div>
+                            <div class="widget-post clearfix">
+                                
+                               
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+         
+
+        </div>
+    </div>
+</div>
+<!-- FOOTER COPYRIGHT -->
+
+<div class="footer-middle">
+
+    <div class="container">
+        <div class="footer-middle-content">
+            <div class="logo-footer">
+                <a href="index.html"><img src="images/logo-light.png" alt=""></a>
+                
+            </div>
+           
+            <ul class="social-icons  wt-social-links footer-social-icon">
+                <li><a href="https://www.facebook.com/riotu.center" class="fa fa-facebook"></a></li>
+                <li><a href="https://twitter.com/riotulab" class="fa fa-twitter"></a></li>
+                <li><a href="http://linkedin.com/in/anis-koubaa-84aa4344" class="fa fa-linkedin"></a></li>
+            </ul>
+
+           
+        </div>
+    </div>
+
+</div>
+<div class="footer-bottom">
+    <div class="container">
+        <div class="wt-footer-bot-left d-flex justify-content-center">
+            <span class="copyrights-text">© 2020 RIOTU.</span>
+        </div>
+    </div>
+</div>
+
+
+</footer>
+<!-- FOOTER END -->
+
+<!-- BUTTON TOP START -->
+<button class="scroltop"><span class="fa fa-angle-up  relative" id="btn-vibrate"></span></button>
+
+</div>
+
+<!-- LOADING AREA START ===== -->
+<div class="loading-area">
+<div class="loading-box"></div>
+<div class="loading-pic">
+<div class="cssload-loader"></div>
+</div>
+</div>
+    <!-- LOADING AREA  END ====== -->
+
+    <!-- JAVASCRIPT  FILES ========================================= -->
+    <script src="js/jquery-2.2.0.min.js"></script><!-- JQUERY.MIN JS -->
+    <script src="js/popper.min.js"></script><!-- POPPER.MIN JS -->
+    <script src="js/bootstrap.min.js"></script><!-- BOOTSTRAP.MIN JS -->
+    <script src="js/bootstrap-select.min.js"></script><!-- Form js -->
+    <script src="js/magnific-popup.min.js"></script><!-- MAGNIFIC-POPUP JS -->
+    <script src="js/waypoints.min.js"></script><!-- WAYPOINTS JS -->
+    <script src="js/counterup.min.js"></script><!-- COUNTERUP JS -->
+    <script src="js/waypoints-sticky.min.js"></script><!-- STICKY HEADER -->
+    <script src="js/isotope.pkgd.min.js"></script><!-- MASONRY  -->
+    <script src="js/owl.carousel.min.js"></script><!-- OWL  SLIDER  -->
+    <script src="js/stellar.min.js"></script><!-- PARALLAX BG IMAGE   -->
+    <script src="js/theia-sticky-sidebar.js"></script><!-- STICKY SIDEBAR  -->
+    <script src="js/jquery.bootstrap-touchspin.js"></script><!-- FORM JS -->
+    <script src="js/custom.js"></script><!-- CUSTOM FUCTIONS  -->
+    <script src="js/jquery.bgscroll.js"></script><!-- BACKGROUND SCROLL -->
+    <script src="js/slick.js"></script>
+    <script src="js/jquery-ui.js"></script>
+
+
+
+
+
+
+    <!-- REVOLUTION JS FILES -->
+
+    <script src="plugins/revolution/revolution/js/jquery.themepunch.tools.min.js"></script>
+    <script src="plugins/revolution/revolution/js/jquery.themepunch.revolution.min.js"></script>
+
+    <!-- SLIDER REVOLUTION 5.0 EXTENSIONS  (Load Extensions only on Local File Systems !  The following part can be removed on Server for On Demand Loading) -->
+    <script src="plugins/revolution/revolution/js/extensions/revolution-plugin.js"></script>
+
+    <!-- REVOLUTION SLIDER SCRIPT FILES -->
+    <script src="js/rev-script-3.js"></script>
+
+
+
+
+
+</body>
+
+</html>
