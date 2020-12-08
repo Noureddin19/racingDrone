9bbab13f2a6650c3a3275ae275beac05fdd10514
Noureddin19 <49112495+Noureddin19@users.noreply.github.com>
2020-12-08 10:45:44 +0300

    H

--- login.php
+++ login.php
@@ -9,6 +9,16 @@ session_start();
 
 <head>
 
+
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-GG4JCWDBMK"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-GG4JCWDBMK');
+</script>
     <!-- META -->
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
