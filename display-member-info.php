534793fc8775858573356d61c8497087685e99f3
Noureddin19 <49112495+Noureddin19@users.noreply.github.com>
2020-12-09 10:47:00 +0300

    s

--- display-member-info.php
+++ display-member-info.php
@@ -1,7 +1,5 @@
 <?php 
 session_start();
-$_SESSION['register'] = false;
-header("Location: index.php");
 
 require('sql/connect.php');
 $_SESSION["unname"] = $_POST['uname'];
@@ -211,7 +209,7 @@ $_SESSION['dow']= $_POST['dow'];
         <div class="row">
 <div class="col-md-3"></div>
   <div class="col-md-3">
-    <form action="" method="POST">
+    <form action="add-membership-info.php" method="POST">
     <center><button type="submit" name="submit" value ="submit" class="site-button site-btn-effect border border-danger justify-content-center mt-4 px-0" style="width: 90%">Submit</button></center>
     </form>
   </div>
@@ -348,42 +346,7 @@ $_SESSION['dow']= $_POST['dow'];
         
             </div>
         <!-- CONTENT END -->
-    <?php
-        if(isset($_POST['submit'])){
-    #PHP $_REQUEST is a PHP super global variable which is used to collect data after submitting an HTML form
-    $uname = $_POST['uname'];
-    $phoneNumber = $_POST['pn'];
-    $email = $_POST['email'];
-    $age = $_POST['age'];
-    $fcn = $_POST['fcn'];
-    $org = $_POST['org'];
-    $ea = $_POST['ea'];
-    $nationality =$_POST['nationality'];
-    $ms = $_POST['ms'];
-    $bcn = $_POST['bcn'];
-    $bcc = $_POST['bcc'];
-    $dow = $_POST['dow'];
-    $ins_query="insert into membership_form(fullname,age, email, phonenumber, flightcontrollername, organization, nationality, escamps, motorssizeandkv,batterycellsnumber, batterycellscapacity, overallweight)values('$uname','$age', '$email','$phoneNumber', '$fcn','$org', '$nationality','$ea', '$ms' ,'$bcn', '$bcc','$dow' )";
     
-    
-   
-        try{
-            $pdo->exec($ins_query);
-    $status = $uname. " your Record is added Successfully ";
-    ob_start();
-    $_SESSION['register'] = true;
-
-    exit();
-   
-        }catch(PDOException $e) {
-            echo "database failed: " . $e->getMessage();
-    
-        }
-}else{
-    echo "reFills the form";
-}
-
-?>
          <!-- FOOTER START -->
          <footer class="site-footer footer-large footer-dark text-white footer-style1">
 
