7d06d8fe02c8c2e6d1fdd7e598f25f3b129c9b34
Noureddin19 <49112495+Noureddin19@users.noreply.github.com>
2020-12-16 19:24:47 +0300

    df

--- admin/pages/general-table.php
+++ admin/pages/general-table.php
@@ -149,8 +149,8 @@ header("location: ../../login.php");
                                 </div>
                             </li>
                             <li class="nav-item">
-                                <a class="nav-link" href="#" data-toggle="collapse" aria-expanded="false" data-target="#submenu-4" aria-controls="submenu-4"><i class="fas fa-fw fa-table"></i>Content</a>
-                                <div id="submenu-4" class="collapse submenu" style="">
+                                <a class="nav-link" href="#" data-toggle="collapse" aria-expanded="false" data-target="#submenu-6" aria-controls="submenu-6"><i class="fas fa-fw fa-table"></i>Content</a>
+                                <div id="submenu-6" class="collapse submenu" style="">
                                     <ul class="nav flex-column">
                                         <li class="nav-item">
                                             <a class="nav-link" href="#">club content</a>
