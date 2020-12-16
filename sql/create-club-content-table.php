623d2c52b639f3322de30d9535faee6064d6c5ee
Noureddin19 <49112495+Noureddin19@users.noreply.github.com>
2020-12-16 17:45:59 +0300

    Create create-club-content-table.php

---
+++ sql/create-club-content-table.php
@@ -0,0 +1,22 @@
+<?php
+    require('connect.php');
+    
+   
+    #step5 : create the table
+    $sql = "CREATE TABLE IF NOT EXISTS membership_form(
+    id int(12) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    type VARCHAR(30),
+    content VARCHAR(12),
+)";
+    
+    //step6 : run the query
+   try{
+    $pdo->exec($sql);
+    //echo "<br>";
+    //echo "<h3>Table Info created successfuly</h3>";
+   }catch(PDOException $e) {
+    echo "table failed: " . $e->getMessage();
+}
+    
+    ?>
+    
