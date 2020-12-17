5ac6b7b134ae4a0657f4916ae240756864d7f1af
Noureddin19 <49112495+Noureddin19@users.noreply.github.com>
2020-12-17 22:04:00 +0300

    dd

--- index.php
+++ index.php
@@ -470,32 +470,9 @@ if($_SESSION['register']){
     <!-- OUR TEAM SECTION END -->
             
             <!-- CLIENT LOGO SECTION START -->
-            <div class="section-full bg-gray p-t50 p-b50 client-logo-outer-one">
-                <div class="container-fluid">
-
-                    <div class="section-content p-tb30 owl-btn-vertical-center">
-                        <div class="owl-carousel home-client-carousel-2">
-
-                            <?php
-
-                            foreach(glob("images/sponsors/{*.gif,*.jpg,*.png,*.jpeg,*.bmp}", GLOB_BRACE) as $image){
-                                echo '<div class="item">
-                                <div class="ow-client-logo">
-                                    <div class="client-logo client-logo-media">
-                                        <a href="javascript:void(0);"><img src="'.$image.'" alt=""></a>
-                                    </div>
-                                </div>
-                            </div>';
-                            } 
-
-                            ?>
-
-                           
-                        </div>
-                    </div>
-
-                </div>
-            </div>
+            <?php
+           include("includes/sponsers.php");
+           ?>
             <!-- CLIENT LOGO  SECTION End -->
                                       
         </div>
