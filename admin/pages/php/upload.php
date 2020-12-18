37250685de93678cacfe1bb4eabb4163ba4fa7c4
Talal Bafarat <tbafarat@gmail.com>
2020-12-18 03:12:04 +0300

    made kwayes

--- admin/pages/php/upload.php
+++ admin/pages/php/upload.php
@@ -6,8 +6,6 @@ $statusMsg = '';
 // File upload path
 $target_dir = "../../../images/sponsors/";
 $target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
-echo($target_file." is the image");
-echo("<br>");
 $uploadOk = 1;
 $imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
 
@@ -15,7 +13,7 @@ $imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
 if(isset($_POST["submit"])) {
   $check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
   if($check !== false) {
-    echo "File is an image - " . $check["mime"] . ".";
+    // echo "File is an image - " . $check["mime"] . ".";
     $uploadOk = 1;
   } else {
     echo "File is not an image.";
@@ -48,7 +46,8 @@ if ($uploadOk == 0) {
 // if everything is ok, try to upload file
 } else {
   if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
-    echo "The file ". htmlspecialchars( basename( $_FILES["fileToUpload"]["name"])). " has been uploaded.";
+    // echo "The file ". htmlspecialchars( basename( $_FILES["fileToUpload"]["name"])). " has been uploaded.";
+    header("location: ../sponsors-table.php");
   } else {
     echo "Sorry, there was an error uploading your file.";
   }
