7bdd7c59820175339698f2b950d135e8e3dd0ba2
Noureddin19 <49112495+Noureddin19@users.noreply.github.com>
2020-12-18 04:45:55 +0300

    sdf

---
+++ admin/pages/php/delete-comitee.php
@@ -0,0 +1,31 @@
+<?php
+
+include '../../../sql/connect.php';
+$id = $_GET['id']; // get is super global
+
+
+
+
+try{
+$stmt = $pdo->prepare("DELETE FROM compition_commitee where id='$id';");
+$stmt->execute();
+try{
+  
+
+$notification = "7";
+$date = date("Y-m-d h:i");
+
+$nof_qury = "INSERT INTO `notification` (`username`, `notification`, `creation`) VALUES ('d', '$notification', '$date')";
+$pdo->exec($nof_qury);
+}catch(PDOException $e) {
+  echo "database failed: " . $e->getMessage();
+
+}
+header('Location: ../data-tables.php');
+}catch(PDOException $e) {
+	echo "Error: " . $e->getMessage();
+  }
+
+
+
+?>
