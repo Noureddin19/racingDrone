61b0195652b46eea91d488966a21d916fe7bc3d0
Naif Alfirm <Nfahad40@gmail.com>
2020-12-18 14:54:21 +0300

    Update general-table.php

--- admin/pages/general-table.php
+++ admin/pages/general-table.php
@@ -7,48 +7,28 @@ require('../../sql/connect.php');
 $stmt = $pdo->prepare("SELECT * FROM membership_form;");
 $stmt->execute();
 $i=0;
-$y
+$saudi= 0;
+$non =0;
 while($row = $stmt->fetch(PDO::FETCH_ASSOC)){
 $i++;
-
+if(!strcasecmp($row['nationality'],'Saudi')){
+  
+$saudi++;
+}else{
+   
+    $non++;
+}
 }
 
 $totalVisitors = $i;
  
-$newVsReturningVisitorsDataPoints = array(
-	array("y"=> 519960, "name"=> "Saudi", "color"=> "#E7823A"),
-	array("y"=> 363040, "name"=> "Non Saudi", "color"=> "#546BC1")
+$dataPoints = array(
+	array("x"=> 1, "y"=> $totalVisitors, "indexLabel"=> "TotalRegisters" ),
+	array("x"=> 2, "y"=> $saudi, "indexLabel"=> "Saudi Registers"),
+    array("x"=> 3, "y"=> $non, "indexLabel"=> "Non Saudi Registers")
+	
 );
- 
-$newVisitorsDataPoints = array(
-	array("x"=> 1420050600000 , "y"=> 33000),
-	array("x"=> 1422729000000 , "y"=> 35960),
-	array("x"=> 1425148200000 , "y"=> 42160),
-	array("x"=> 1427826600000 , "y"=> 42240),
-	array("x"=> 1430418600000 , "y"=> 43200),
-	array("x"=> 1433097000000 , "y"=> 40600),
-	array("x"=> 1435689000000 , "y"=> 42560),
-	array("x"=> 1438367400000 , "y"=> 44280),
-	array("x"=> 1441045800000 , "y"=> 44800),
-	array("x"=> 1443637800000 , "y"=> 48720),
-	array("x"=> 1446316200000 , "y"=> 50840),
-	array("x"=> 1448908200000 , "y"=> 51600)
-);
- 
-$returningVisitorsDataPoints = array(
-	array("x"=> 1420050600000 , "y"=> 22000),
-	array("x"=> 1422729000000 , "y"=> 26040),
-	array("x"=> 1425148200000 , "y"=> 25840),
-	array("x"=> 1427826600000 , "y"=> 23760),
-	array("x"=> 1430418600000 , "y"=> 28800),
-	array("x"=> 1433097000000 , "y"=> 29400),
-	array("x"=> 1435689000000 , "y"=> 33440),
-	array("x"=> 1438367400000 , "y"=> 37720),
-	array("x"=> 1441045800000 , "y"=> 35200),
-	array("x"=> 1443637800000 , "y"=> 35280),
-	array("x"=> 1446316200000 , "y"=> 31160),
-	array("x"=> 1448908200000 , "y"=> 34400)
-);
+	
  
  
 ?>
@@ -69,97 +49,25 @@ $returningVisitorsDataPoints = array(
 <script>
 window.onload = function () {
  
-var totalVisitors = <?php echo $totalVisitors ?>;
-var visitorsData = {
-	"New vs Returning Visitors": [{
-		click: visitorsChartDrilldownHandler,
-		cursor: "pointer",
-		explodeOnClick: false,
-		innerRadius: "75%",
-		legendMarkerType: "square",
-		name: "New vs Returning Visitors",
-		radius: "100%",
-		showInLegend: true,
-		startAngle: 90,
-		type: "doughnut",
-		dataPoints: <?php echo json_encode($newVsReturningVisitorsDataPoints, JSON_NUMERIC_CHECK); ?>
-	}],
-	"New Visitors": [{
-		color: "#E7823A",
-		name: "New Visitors",
-		type: "column",
-		xValueType: "dateTime",
-		dataPoints: <?php echo json_encode($newVisitorsDataPoints, JSON_NUMERIC_CHECK); ?>
-	}],
-	"Returning Visitors": [{
-		color: "#546BC1",
-		name: "Returning Visitors",
-		type: "column",
-		xValueType: "dateTime",
-		dataPoints: <?php echo json_encode($returningVisitorsDataPoints, JSON_NUMERIC_CHECK); ?>
+var chart = new CanvasJS.Chart("chartContainer", {
+	animationEnabled: true,
+	exportEnabled: true,
+	theme: "light1", // "light1", "light2", "dark1", "dark2"
+	title:{
+		text: "Simple Column Chart with Index Labels"
+	},
+	axisY:{
+		includeZero: true
+	},
+	data: [{
+		type: "column", //change type to bar, line, area, pie, etc
+		//indexLabel: "{y}", //Shows y value on all Data Points
+		indexLabelFontColor: "#5A5757",
+		indexLabelPlacement: "outside",   
+		dataPoints: <?php echo json_encode($dataPoints, JSON_NUMERIC_CHECK); ?>
 	}]
-};
- 
-var newVSReturningVisitorsOptions = {
-	animationEnabled: true,
-	theme: "light2",
-	title: {
-		text: "Saudi VS Non Saudi"
-	},
-	subtitles: [{
-		text: "Click on Any Segment to Drilldown",
-		backgroundColor: "#2eacd1",
-		fontSize: 16,
-		fontColor: "white",
-		padding: 5
-	}],
-	legend: {
-		fontFamily: "calibri",
-		fontSize: 14,
-		itemTextFormatter: function (e) {
-			return e.dataPoint.name + ": " + totalVisitors + "";  
-		}
-	},
-	data: []
-};
- 
-var visitorsDrilldownedChartOptions = {
-	animationEnabled: true,
-	theme: "light2",
-	axisX: {
-		labelFontColor: "#717171",
-		lineColor: "#a2a2a2",
-		tickColor: "#a2a2a2"
-	},
-	axisY: {
-		gridThickness: 0,
-		includeZero: false,
-		labelFontColor: "#717171",
-		lineColor: "#a2a2a2",
-		tickColor: "#a2a2a2",
-		lineThickness: 1
-	},
-	data: []
-};
- 
-var chart = new CanvasJS.Chart("chartContainer", newVSReturningVisitorsOptions);
-chart.options.data = visitorsData["New vs Returning Visitors"];
-chart.render();
- 
-function visitorsChartDrilldownHandler(e) {
-	chart = new CanvasJS.Chart("chartContainer", visitorsDrilldownedChartOptions);
-	chart.options.data = visitorsData[e.dataPoint.name];
-	chart.options.title = { text: e.dataPoint.name }
-	chart.render();
-	$("#backButton").toggleClass("invisible");
-}
- 
-$("#backButton").click(function() { 
-	$(this).toggleClass("invisible");
-	chart = new CanvasJS.Chart("chartContainer", newVSReturningVisitorsOptions);
-	chart.options.data = visitorsData["New vs Returning Visitors"];
-	chart.render();
 });
+chart.render();
  
 }
 </script>
@@ -400,29 +308,10 @@ $("#backButton").click(function() {
                                     <table class="table table-striped">
                                         <thead>
                                             <tr><div id="chartContainer" style="height: 370px; width: 100%;"></div>
-<button class="btn invisible" id="backButton">&lt; Back</button>
+
 </tr>
                                         </thead>
-                                        <tbody>
-                                            <tr>
-                                                <th scope="row">1</th>
-                                                <td>Mark</td>
-                                                <td>Otto</td>
-                                                <td>@mdo</td>
-                                            </tr>
-                                            <tr>
-                                                <th scope="row">2</th>
-                                                <td>Jacob</td>
-                                                <td>Thornton</td>
-                                                <td>@fat</td>
-                                            </tr>
-                                            <tr>
-                                                <th scope="row">3</th>
-                                                <td>Larry</td>
-                                                <td>the Bird</td>
-                                                <td>@twitter</td>
-                                            </tr>
-                                        </tbody>
+                                        
                                     </table>
                                 </div>
                             </div>
@@ -640,8 +529,8 @@ $("#backButton").click(function() {
     <script src="../assets/vendor/slimscroll/jquery.slimscroll.js"></script>
     <script src="../assets/vendor/custom-js/jquery.multi-select.html"></script>
     <script src="../assets/libs/js/main-js.js"></script>
-    <script src="https://canvasjs.com/assets/script/jquery-1.11.1.min.js"></script>
-<script src="https://canvasjs.com/assets/script/canvasjs.min.js"></script>
+    <script src="https://canvasjs.com/assets/script/canvasjs.min.js"></script>
+
 
 </body>
  
