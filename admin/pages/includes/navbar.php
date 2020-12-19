6c6e144a4415eec18d7f4a94e330f5db3e759590
Naif Alfirm <Nfahad40@gmail.com>
2020-12-19 14:44:47 +0300

    a

--- admin/pages/includes/navbar.php
+++ admin/pages/includes/navbar.php
@@ -4,10 +4,10 @@
             <nav class="navbar navbar-expand-lg bg-white fixed-top">
                 <a class="navbar-brand" href="../pages/index.php">Admin</a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                    <span class="navbar-toggler-icon"></span>
+                    <i class="navbar-toggler-icon rounded-circle" style="color:blue;">ME</i>
                 </button>
 
-                <div class="collapse navbar-collapse " id="navbarSupportedContent">
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
                     <ul class="navbar-nav ml-auto navbar-right-top">
                       
                         <li class="nav-item dropdown notification">
