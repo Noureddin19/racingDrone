b08998bb1b48d613adbf27662abbb3a8df6a9c85
Noureddin19 <49112495+Noureddin19@users.noreply.github.com>
2020-12-22 20:19:29 +0300

    يس

--- admin/pages/php/update-club.php
+++ admin/pages/php/update-club.php
@@ -1,12 +1,18 @@
 <?php 
 session_start();
 
+// connect data base
+
 require('../../../sql/connect.php');
 if(!$_SESSION['logged']){
+    // check if the user is logged in
+
 header("location:../ ../../login.php");
 }
 ?>
 <?php 
+// update club-Content table by getting the info from club-Content-table.php
+
 $new_contents = $_POST["contents"];
 $type = $_GET["type"];
 $stmt = $pdo->prepare("UPDATE club_content_table SET content='$new_contents' WHERE type='$type';");
--- admin/pages/php/update-comptiton.php
+++ admin/pages/php/update-comptiton.php
@@ -1,14 +1,21 @@
 <?php 
 session_start();
+// connect data base
 
 require('../../../sql/connect.php');
 if(!$_SESSION['logged']){
+// check if the user is logged in
+
 header("location:../ ../../login.php");
 }
 ?>
 <?php 
+// update compition-content table by getting the info from compition-content-table.php
+
 $new_contents = $_POST["contents"];
 $type = $_GET["type"];
+// get type from link
+
 $stmt = $pdo->prepare("UPDATE compition_content_table SET content='$new_contents' WHERE type='$type';");
         $stmt->execute();
 		header('Location: ../compition-table.php');
