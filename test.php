d72568b27012f0f98cec5606026aa96883f044bc
Noureddin19 <49112495+Noureddin19@users.noreply.github.com>
2020-12-22 17:08:57 +0300

    fds

--- test.php
+++
@@ -1,5 +0,0 @@
-<?php 
-    mail('nfahad40@gmail.com', 'Subject Line Here', 'Body of Message Here', 'From: racing@psu.edu.sa');
-    mail('tbafarat@gmail.com', 'Subject Line Here', 'Body of Message Here', 'From: racing@psu.edu.sa');
-    mail('aldhbeg@gmail.com', 'sadf', 'asdsa', 'From: racing@psu.edu.sa');
-                    ?>
