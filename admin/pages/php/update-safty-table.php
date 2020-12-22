7ae29bec4ac100000ff2fa00a59793d251a2e483
Noureddin19 <49112495+Noureddin19@users.noreply.github.com>
2020-12-22 20:10:48 +0300

    يب

--- admin/pages/php/update-safty-table.php
+++ admin/pages/php/update-safty-table.php
@@ -7,8 +7,10 @@ header("location:../ ../../login.php");
 }
 ?>
 <?php 
+// update safty table by getting the info from safty-table.php
 $safty = $_POST["safty"];
 $id = $_GET["id"];
+// get id from link
 $detail = $_POST["detail"];
 $stmt = $pdo->prepare("UPDATE safty SET safty='$safty',detail='$detail' WHERE id='$id';");
         $stmt->execute();
